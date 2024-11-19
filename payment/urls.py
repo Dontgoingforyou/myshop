8b89542c5ab4bf66979a8a98af7e17ccf01c4e41
Dontgoingforyou <mishael000@yandex.ru>
2024-11-19 14:18:50 +0300

    implemented the use of web interceptors to receive payment notifications

--- payment/urls.py
+++ payment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from payment.apps import PaymentConfig
 from payment.views import payment_process, payment_completed, payment_canceled
+from payment.webhooks import stripe_webhook
 
 app_name = PaymentConfig.name
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('process/', payment_process, name='process'),
     path('completed/', payment_completed, name='completed'),
     path('canceled/', payment_canceled, name='canceled'),
+    path('webhook/', stripe_webhook, name='stripe-webhook'),
 ]
